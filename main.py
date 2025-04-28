0f11483131424f96ae908a8694a9b2d87cd2419e
hacker-3342 <hectoruru@outlook.es>
2025-04-28 21:56:21 +0200

    Ported app to PyQt6 for better looks with dark mode and success pop-up. Also added some new features for Linux, but the app isn't fully functional.

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
-import tkinter as tk
-from tkinter import ttk
+import sys
 import os
-import shutil
-import threading
-import configparser
 import json
+import configparser
+import threading
+import shutil
 import glob
 
-# This is a change
+from PyQt6.QtWidgets import (
+    QApplication, QMainWindow, QWidget, QLabel, QPushButton, QVBoxLayout,
+    QHBoxLayout, QToolBar, QComboBox, QCheckBox, QProgressBar, QFrame, QMessageBox
+)
+from PyQt6.QtCore import Qt
 
-class TempFileDeleter:
-    def __init__(self, window):
-        self.window = window
-        self.username = os.environ.get("USERNAME")
-        self.files_deleted = 0
-        self.folders_deleted = 0
+class TempFileDeleter(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Temp File Deleter")
+        self.resize(700, 500)
+
+        self.username = os.environ.get("USERNAME") or os.environ.get("USER")
         self.config = configparser.ConfigParser()
         self.config_file = os.path.join(os.path.dirname(__file__), 'settings.ini')
         self.load_language()
-        self.setup_ui()
-        self.load_checkbox_states()
-        self.toggle_checkboxes()
+
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+
+        self.layout = QVBoxLayout(self.central_widget)
+
+        self.create_toolbar()
+        self.create_ui()
+
+        self.files_deleted = 0
+        self.folders_deleted = 0
 
     def load_language(self):
         self.config.read(self.config_file)
@@ -29,188 +41,145 @@ class TempFileDeleter:
         with open(language_file, 'r') as f:
             self.lang = json.load(f)
 
-    def setup_ui(self):
-        self.window.geometry("700x500")
-        self.create_sidebar()
-        name = tk.Label(self.window, text=self.lang["title"], font=("Helvetica", 20))
-        name.pack(padx=10, pady=1)
-        desc = tk.Label(self.window, text=self.lang["description"], font=("Helvetica", 10))
-        desc.pack(padx=10, pady=1)
-        self.create_checkboxes()
-        self.create_buttons()
-        self.create_advisory_label()
+    def create_toolbar(self):
+        toolbar = QToolBar()
+        self.addToolBar(toolbar)
 
-    def create_sidebar(self):
-        self.sidebar_frame = tk.Frame(self.window)
-        self.sidebar_frame.pack(side=tk.LEFT, fill=tk.Y, padx=10, pady=10)
-        self.create_language_dropdown(self.sidebar_frame)
-        self.create_save_button(self.sidebar_frame)
-
-    def create_checkboxes(self):
-        self.checkbox_frame = tk.Frame(self.window)
-        self.checkbox_frame.pack(padx=10, pady=10)
-        self.path_vars = []
-
-        self.d_drive_var, self.d_drive_checkbox = self.create_checkbox(self.checkbox_frame, self.lang["d_drive"], padx=10)
-        self.d_ame_var, self.d_ame_checkbox = self.create_checkbox(self.checkbox_frame, self.lang["d_ame"], padx=30)
-        self.d_extensions_var, self.d_extensions_checkbox = self.create_checkbox(self.checkbox_frame, self.lang["d_extensions"], padx=30)
-
-        self.c_drive_var, self.c_drive_checkbox = self.create_checkbox(self.checkbox_frame, self.lang["c_drive"], padx=10)
-        self.c_ame_var, self.c_ame_checkbox = self.create_checkbox(self.checkbox_frame, self.lang["c_ame"], padx=30)
-        self.c_extensions_var, self.c_extensions_checkbox = self.create_checkbox(self.checkbox_frame, self.lang["c_extensions"], padx=30)
-
-        self.e_drive_var = self.create_checkbox(self.checkbox_frame, self.lang["e_drive"], padx=10)[0]
-        self.adobe_cache_var = self.create_checkbox(self.checkbox_frame, self.lang["adobe_cache"].format(username=self.username), padx=10)[0]
-
-        self.d_drive_var.trace_add('write', lambda *args: self.toggle_suboptions(self.d_drive_var, [(self.d_ame_var, self.d_ame_checkbox), (self.d_extensions_var, self.d_extensions_checkbox)]))
-        self.c_drive_var.trace_add('write', lambda *args: self.toggle_suboptions(self.c_drive_var, [(self.c_ame_var, self.c_ame_checkbox), (self.c_extensions_var, self.c_extensions_checkbox)]))
-
-    def create_buttons(self):
-        self.btn_frame = tk.Frame(self.window)
-        self.btn_frame.pack(padx=10, pady=10)
-        self.btn = tk.Button(self.btn_frame, text=self.lang["delete_files"], command=lambda: self.start_deletion(shutdown=False))
-        self.btn.pack(side=tk.LEFT, padx=5)
-        self.btn2 = tk.Button(self.btn_frame, text=self.lang["delete_files_shutdown"], command=lambda: self.start_deletion(shutdown=True))
-        self.btn2.pack(side=tk.LEFT, padx=5)
-
-    def create_save_button(self, parent):
-        self.save_btn = tk.Button(parent, text=self.lang["save_settings"], command=self.save_settings)
-        self.save_btn.pack(padx=10, pady=2)
-
-    def save_settings(self):
-        self.save_checkbox_states()
-        self.config['Settings'] = {'language': self.language_var.get()}
-        with open(self.config_file, 'w') as configfile:
-            self.config.write(configfile)
-        print("Settings saved")
-
-    def create_advisory_label(self):
-        advisory = tk.Label(self.window, text=self.lang["advisory"], font=("Helvetica", 10))
-        advisory.pack(padx=10, pady=1)
-
-    def create_checkbox(self, parent, text, padx=20):
-        var = tk.BooleanVar()
-        checkbox = tk.Checkbutton(parent, text=text, variable=var)
-        checkbox.pack(anchor='w', padx=padx)
-        self.path_vars.append(var)
-        return var, checkbox
-
-    def toggle_suboptions(self, main_var, sub_vars):
-        state = tk.NORMAL if main_var.get() else tk.DISABLED
-        for var in sub_vars:
-            var[1].config(state=state)
-
-    def start_deletion(self, shutdown=False):
-        self.save_checkbox_states()
-        self.btn.config(state=tk.DISABLED)
-        self.btn2.config(state=tk.DISABLED)
-        self.progress_bar = ttk.Progressbar(self.window, mode='indeterminate')
-        self.progress_bar.pack(padx=10, pady=10, fill=tk.X)
-        self.separator = ttk.Separator(self.window, orient='horizontal')
-        self.separator.pack(fill='x', padx=10, pady=10)
-        labels_frame = tk.Frame(self.window)
-        labels_frame.pack(padx=10, pady=1)
-        self.label_folders = tk.Label(labels_frame, text=self.lang["folders_deleted"].format(count=self.folders_deleted), bg="yellow")
-        self.label_folders.pack(side=tk.LEFT, padx=5)
-        self.label_files = tk.Label(labels_frame, text=self.lang["files_deleted"].format(count=self.files_deleted), bg="red")
-        self.label_files.pack(side=tk.LEFT, padx=5)
-        self.label_completed = tk.Label(labels_frame, text="")
-        self.label_completed.pack(side=tk.LEFT, padx=5)
-        threading.Thread(target=self.delete_files, args=(shutdown,)).start()
-
-    def delete_files(self, shutdown=False):
-        print("Deleting temporary files...")
-        self.progress_bar.start()
-        paths = [
-            ("D:/", "_AME", self.d_ame_var), ("C:/", "_AME", self.c_ame_var),
-            ("D:/", [".mpgindex", ".ims", ".cfa", ".pek"], self.d_extensions_var),
-            ("C:/", [".mpgindex", ".ims", ".cfa", ".pek"], self.c_extensions_var),
-            ("E:/", None, self.e_drive_var),
-            (f"C:/Users/{self.username}/AppData/Roaming/Adobe/common/Media Cache Files", None, self.adobe_cache_var)
-        ]
-        paths = [(path, condition) for path, condition, var in paths if var.get()]
-        for path, condition in paths:
-            self.label_completed.config(text=self.lang["searching_in"].format(path=path), bg="orange")
-            self.delete_path(path, condition)
-            self.window.update_idletasks()
-        print("Files deleted")
-        self.progress_bar.stop()
-        self.progress_bar.config(value=100)
-        self.label_completed.config(text=self.lang["operation_completed"], bg="green", width=30)
-        if shutdown:
-            print("Shutting down the computer...")
-            os.system("shutdown /s /t 1")
-
-    def delete_path(self, path, condition=None):
-        for root, subdirs, files in os.walk(path, topdown=False):
-            if condition is None:
-                for name in files:
-                    try:
-                        os.remove(os.path.join(root, name))
-                        self.files_deleted += 1
-                        self.label_files.config(text=self.lang["files_deleted"].format(count=self.files_deleted))
-                    except PermissionError:
-                        pass
-                for name in subdirs:
-                    try:
-                        shutil.rmtree(os.path.join(root, name))
-                        self.folders_deleted += 1
-                        self.label_folders.config(text=self.lang["folders_deleted"].format(count=self.folders_deleted))
-                    except PermissionError:
-                        pass
-            elif isinstance(condition, str):
-                for name in subdirs:
-                    if name.endswith(condition):
-                        try:
-                            shutil.rmtree(os.path.join(root, name))
-                            self.folders_deleted += 1
-                            self.label_folders.config(text=self.lang["folders_deleted"].format(count=self.folders_deleted))
-                        except PermissionError:
-                            pass
-            elif isinstance(condition, list):
-                for name in files:
-                    if any(name.endswith(ext) for ext in condition):
-                        try:
-                            os.remove(os.path.join(root, name))
-                            self.files_deleted += 1
-                            self.label_files.config(text=self.lang["files_deleted"].format(count=self.files_deleted))
-                        except PermissionError:
-                            pass
-
-    def toggle_checkboxes(self):
-        self.d_drive_var.set(True)
-        self.d_drive_var.set(False)
-        self.c_drive_var.set(True)
-        self.c_drive_var.set(False)
-
-    def save_checkbox_states(self):
-        self.config['Checkboxes'] = {f'var{i}': str(var.get()) for i, var in enumerate(self.path_vars)}
-        with open(self.config_file, 'w') as configfile:
-            self.config.write(configfile)
-
-    def load_checkbox_states(self):
-        if os.path.exists(self.config_file):
-            self.config.read(self.config_file)
-            for i, var in enumerate(self.path_vars):
-                var.set(self.config.getboolean('Checkboxes', f'var{i}', fallback=False))
-
-    def create_language_dropdown(self, parent):
+        # Language dropdown
+        self.language_combo = QComboBox()
         languages_dir = os.path.join(os.path.dirname(__file__), 'Languages')
         languages = [os.path.basename(f).split('.')[0] for f in glob.glob(os.path.join(languages_dir, '*.json'))]
-        self.language_var = tk.StringVar(value=self.config.get('Settings', 'language', fallback='english'))
-        dropdown = ttk.Combobox(parent, textvariable=self.language_var, values=languages)
-        dropdown.pack(padx=10, pady=2)
-        dropdown.bind("<<ComboboxSelected>>", self.change_language)
+        self.language_combo.addItems(languages)
+        self.language_combo.setCurrentText(self.config.get('Settings', 'language', fallback='english'))
+        self.language_combo.currentIndexChanged.connect(self.change_language)
 
-    def change_language(self, event):
-        self.config['Settings'] = {'language': self.language_var.get()}
+        toolbar.addWidget(self.language_combo)
+
+        # Save settings button
+        save_button = QPushButton(self.lang["save_settings"])
+        save_button.clicked.connect(self.save_settings)
+        toolbar.addWidget(save_button)
+
+    def create_ui(self):
+        # Title
+        title = QLabel(self.lang["title"])
+        title.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        title.setStyleSheet("font-size: 20px;")
+        self.layout.addWidget(title)
+
+        # Description
+        desc = QLabel(self.lang["description"])
+        desc.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        desc.setStyleSheet("font-size: 12px; color: gray;")
+        self.layout.addWidget(desc)
+
+        # Checkboxes
+        self.checkboxes = {}
+        for key in ["d_drive", "d_ame", "d_extensions", "c_drive", "c_ame", "c_extensions", "e_drive", "adobe_cache"]:
+            cb = QCheckBox(self.lang.get(key, key))
+            self.checkboxes[key] = cb
+            self.layout.addWidget(cb)
+
+        # Delete buttons
+        btn_layout = QHBoxLayout()
+        self.delete_btn = QPushButton(self.lang["delete_files"])
+        self.delete_btn.clicked.connect(lambda: self.start_deletion(shutdown=False))
+        btn_layout.addWidget(self.delete_btn)
+
+        self.delete_shutdown_btn = QPushButton(self.lang["delete_files_shutdown"])
+        self.delete_shutdown_btn.clicked.connect(lambda: self.start_deletion(shutdown=True))
+        btn_layout.addWidget(self.delete_shutdown_btn)
+
+        self.layout.addLayout(btn_layout)
+
+        # Separator
+        separator = QFrame()
+        separator.setFrameShape(QFrame.Shape.HLine)
+        separator.setFrameShadow(QFrame.Shadow.Sunken)
+        self.layout.addWidget(separator)
+
+        # Progress bar and labels
+        self.progress_bar = QProgressBar()
+        self.layout.addWidget(self.progress_bar)
+
+        self.status_label = QLabel("")
+        self.layout.addWidget(self.status_label)
+
+    def save_settings(self):
+        self.config['Settings'] = {'language': self.language_combo.currentText()}
+        with open(self.config_file, 'w') as configfile:
+            self.config.write(configfile())
+        QMessageBox.information(self, "Settings", "Settings saved!")
+
+    def change_language(self):
+        self.config['Settings'] = {'language': self.language_combo.currentText()}
         with open(self.config_file, 'w') as configfile:
             self.config.write(configfile)
-        self.load_language()
-        self.window.destroy()
-        self.__init__(tk.Tk())
+        QMessageBox.information(self, "Language", "Language changed! Please restart the app to apply.")
+
+    def start_deletion(self, shutdown=False):
+        self.progress_bar.setValue(0)
+        threading.Thread(target=self.delete_files, args=(shutdown,), daemon=True).start()
+
+        def delete_files(self, shutdown=False):
+        # Simulate work (replace with your real deletion code later)
+            import time
+            for i in range(101):
+                time.sleep(0.02)
+                self.progress_bar.setValue(i)
+
+            # When done:
+            self.progress_bar.setValue(100)
+            self.status_label.setText(self.lang.get("operation_completed", "Operation completed."))
+        
+            # Show success popup
+            QMessageBox.information(self, "Done!", "Files deleted successfully!")
+
+            if shutdown:
+                # In Linux, shutdown via systemctl
+                os.system("systemctl poweroff")
+
 
 if __name__ == "__main__":
-    window = tk.Tk()
-    app = TempFileDeleter(window)
-    window.mainloop()
+    app = QApplication(sys.argv)
+
+    # DARK THEME 🌑
+    dark_stylesheet = """
+        QWidget {
+            background-color: #121212;
+            color: #e0e0e0;
+        }
+        QPushButton {
+            background-color: #333333;
+            color: #e0e0e0;
+            border: 1px solid #555555;
+            padding: 5px;
+        }
+        QPushButton:hover {
+            background-color: #444444;
+        }
+        QProgressBar {
+            border: 1px solid #555;
+            border-radius: 5px;
+            background-color: #333;
+        }
+        QProgressBar::chunk {
+            background-color: #00bcd4;
+            width: 10px;
+        }
+        QToolBar {
+            background-color: #1f1f1f;
+            spacing: 10px;
+        }
+        QComboBox {
+            background-color: #333;
+            color: #e0e0e0;
+            padding: 3px;
+        }
+    """
+    app.setStyleSheet(dark_stylesheet)
+
+    window = TempFileDeleter()
+    window.show()
+    sys.exit(app.exec())
+
